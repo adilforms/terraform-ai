567998c32ed4c22c634500f759b82d572d57480a
Fidimension <adilforms@gmail.com>
2025-05-05 16:02:49 -0400

    Create s3.tf

---
+++ terraform/s3.tf
@@ -0,0 +1,25 @@
+resource "aws_s3_bucket" "ai_my_bucket" {
+  bucket = var.bucket_name
+  acl    = var.acl
+
+  tags = {
+    Environment = var.environment
+    Project     = var.project
+  }
+}
+
+resource "aws_s3_bucket_policy" "ai_my_bucket_policy" {
+  bucket = aws_s3_bucket.ai_my_bucket.id
+  policy = data.aws_iam_policy_document.ai_bucket_policy.json
+}
+
+data "aws_iam_policy_document" "ai_bucket_policy" {
+  statement {
+    actions   = ["s3:GetObject"]
+    resources = ["${aws_s3_bucket.ai_my_bucket.arn}/*"]
+    principals {
+      type        = "AWS"
+      identifiers = [var.allowed_principal]
+    }
+  }
+}
