68254d386238bc6b1f66f40eb30bed89fdb4165d
Fidimension <adilforms@gmail.com>
2025-05-05 15:57:29 -0400

    Update bucket policy for example-bucket

--- terraform/main.tf
+++ terraform/main.tf
@@ -24,3 +24,20 @@
           content_type = "text/html"
         }
         
+resource "aws_s3_bucket_policy" "example-bucket" {
+  bucket = "example-bucket"
+  policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": "*",
+      "Action": "s3:GetObject",
+      "Resource": "arn:aws:s3:::example-bucket/*"
+    }
+  ]
+}
+
+POLICY
+}
