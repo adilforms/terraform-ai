27e94766c4d8c589129edc015ed870ddc4d44282
Fidimension <adilforms@gmail.com>
2025-05-05 21:10:24 -0400

    Update default acl value in variables.tf for my-terraform-s3-bucket-2025

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,31 +1,5 @@
-variable "region" {
-  description = "AWS region to create resources in"
-  type        = string
-  default     = "us-east-1"
-}
-
-variable "bucket_name" {
-  description = "The name of the S3 bucket"
-  type        = string
-}
-
 variable "acl" {
   description = "Canned ACL to apply to the S3 bucket"
   type        = string
-  default     = "private"
-}
-
-variable "environment" {
-  description = "Deployment environment (e.g., dev, staging, prod)"
-  type        = string
-}
-
-variable "project" {
-  description = "Project name for tagging"
-  type        = string
-}
-
-variable "allowed_principal" {
-  description = "AWS account or IAM user/role ARN to allow access"
-  type        = string
+  default     = "public"
 }
